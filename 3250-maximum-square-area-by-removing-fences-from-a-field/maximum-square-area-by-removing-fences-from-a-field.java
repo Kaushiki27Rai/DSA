f3e63876b6510a60d80fe377ed4fa90f279ad100
Kaushiki Rai <151723299+Kaushiki27Rai@users.noreply.github.com>
2026-01-17 00:04:21 +0530

    Time: 410 ms (85.71%) | Memory: 166.2 MB (72.62%) - LeetSync

---
+++ 3250-maximum-square-area-by-removing-fences-from-a-field/maximum-square-area-by-removing-fences-from-a-field.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Solution {
+    private static final long MOD = 1_000_000_007;
+
+    public int maximizeSquareArea(int m, int n, int[] hFences, int[] vFences) {
+        // Include boundary fences
+        int[] h = new int[hFences.length + 2];
+        int[] v = new int[vFences.length + 2];
+
+        h[0] = 1;
+        h[h.length - 1] = m;
+        for (int i = 0; i < hFences.length; i++) {
+            h[i + 1] = hFences[i];
+        }
+
+        v[0] = 1;
+        v[v.length - 1] = n;
+        for (int i = 0; i < vFences.length; i++) {
+            v[i + 1] = vFences[i];
+        }
+
+        Arrays.sort(h);
+        Arrays.sort(v);
+
+        // All possible horizontal gaps
+        Set<Integer> heightDiffs = new HashSet<>();
+        for (int i = 0; i < h.length; i++) {
+            for (int j = i + 1; j < h.length; j++) {
+                heightDiffs.add(h[j] - h[i]);
+            }
+        }
+
+        // Check vertical gaps and maximize square size
+        long maxSide = 0;
+        for (int i = 0; i < v.length; i++) {
+            for (int j = i + 1; j < v.length; j++) {
+                int width = v[j] - v[i];
+                if (heightDiffs.contains(width)) {
+                    maxSide = Math.max(maxSide, width);
+                }
+            }
+        }
+
+        if (maxSide == 0) return -1;
+
+        return (int) ((maxSide * maxSide) % MOD);
+    }
+}
